8218fc1d90efcb54123029501f5fc96433d35c15
cat_dev <emma.lijing0723@gmail.com>
2023-03-10 13:14:32 -0800

    modified:   public/ajax-utils.js 	modified:   public/snippets/about-page.html

--- public/ajax-utils.js
+++ public/ajax-utils.js
@@ -1,5 +1,29 @@
-// E20221027: Updated version without IIFE 
-// first wrote for printing "Hello Yaacov!"
+
+/**
+ * This module defines an 'ajaxUtils' namespace that provides a utility for making Ajax GET requests to a specified URL. 
+
+ * The main function in this module is 'sendGetRequest', which takes in the URL to make the request to, a function to handle the response, 
+ * and a boolean flag indicating whether the response is in JSON format. 
+
+ * Usage:
+ * import ajaxUtils
+ * ajaxUtils.sendGetRequest(url, responseHandler, isJsonResponse)
+
+ * Where:
+ * - url: a string representing the URL to make the GET request to
+ * - responseHandler: a function to handle the response when it is received
+ * - isJsonResponse: a boolean flag indicating whether the response is in JSON format (default is True)
+
+ * The responseHandler function should take a single argument, which will be either a parsed JSON object or 
+ * raw response text depending on the value of the isJsonResponse flag.
+
+ * Note:
+ * - This module requires XMLHttpRequest, which is available in most modern browsers.
+ * - Ajax (using the XMLHttpRequest object is gradually being replaced by functions within JavaScript frameworks and the official Fetch API Standard.) 
+ *   20230310: For future development, update this by using frameworks and the Official fetch api.
+ * - This module assumes that the server returns a response with a status code of 200 if the request is successful. 
+ */
+
 
 // Set up a namespace for our utility
 let ajaxUtils = {};
@@ -17,16 +41,17 @@ ajaxUtils.sendGetRequest =
           responseHandler,
           isJsonResponse);
       };
+    // initialize the request and prepare it to be sent, `true` indicates that the request should be asynchronous
     request.open("GET", requestUrl, true);
+    // If the request is a GET request, the send method is passed null as the argument. If it were a POST request, data would be passed as the argument to the send method.
     request.send(null); // for POST only
   };
 
-// Only calls user provided 'responseHandler'
-// function if response is ready
-// and not an error
+// Only calls user provided 'responseHandler' function if response is ready and not an error
 function handleResponse(request,
   responseHandler,
   isJsonResponse) {
+  //  4 means that the entire response has been received and the request has completed
   if ((request.readyState == 4) &&
     (request.status == 200)) {
 
@@ -43,74 +68,4 @@ function handleResponse(request,
     }
   }
 }
-
-
-
-
-// (function (global) {
-
-//   // Set up a namespace for our utility
-//   var ajaxUtils = {};
-
-
-//   // Returns an HTTP request object
-//   function getRequestObject() {
-//     if (global.XMLHttpRequest) {
-//       return (new XMLHttpRequest());
-//     }
-//     else if (global.ActiveXObject) {
-//       // For very old IE browsers (optional)
-//       return (new ActiveXObject("Microsoft.XMLHTTP"));
-//     }
-//     else {
-//       global.alert("Ajax is not supported!");
-//       return (null);
-//     }
-//   }
-
-
-//   // Makes an Ajax GET request to 'requestUrl'
-//   ajaxUtils.sendGetRequest =
-//     function (requestUrl, responseHandler, isJsonResponse) {
-//       var request = getRequestObject();
-//       request.onreadystatechange =
-//         function () {
-//           handleResponse(request,
-//             responseHandler,
-//             isJsonResponse);
-//         };
-//       request.open("GET", requestUrl, true);
-//       request.send(null); // for POST only
-//     };
-
-
-//   // Only calls user provided 'responseHandler'
-//   // function if response is ready
-//   // and not an error
-//   function handleResponse(request,
-//     responseHandler,
-//     isJsonResponse) {
-//     if ((request.readyState == 4) &&
-//       (request.status == 200)) {
-
-//       // Default to isJsonResponse = true
-//       if (isJsonResponse == undefined) {
-//         isJsonResponse = true;
-//       }
-
-//       if (isJsonResponse) {
-//         responseHandler(JSON.parse(request.responseText));
-//       }
-//       else {
-//         responseHandler(request.responseText);
-//       }
-//     }
-//   }
-
-
-//   // Expose utility to the global object
-//   global.$ajaxUtils = ajaxUtils;
-
-
-// })(window);
 
