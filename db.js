bc23541aaf89691cc38b03f5c6308e392bc3f5cf
cat_dev <emma.lijing0723@gmail.com>
2023-06-29 18:20:58 -0700

    Change to use .env instead of hardcoding

--- db.js
+++ db.js
@@ -1,14 +1,15 @@
+require('dotenv').config(); // Load environment variables from .env file
 
 // The Pool property contains a constructor function for creating a connection pool for a PostgreSQL database.
 const { Pool } = require("pg");
 
 const credentials = {
-    user: "metabase_catwatcher_user",
-    database: "metabase_catwatcher_db",
-    password: "metabase_catwatcher_pw",
-    port: 5432,
-    host: "localhost",
-}
+    user: process.env.DB_USER,
+    database: process.env.DB_DATABASE,
+    password: process.env.DB_PASSWORD,
+    port: process.env.DB_PORT,
+    host: process.env.DB_HOST,
+};
 
 // create a new pool object and use it to manage connections to the database
 const pool = new Pool(credentials);
